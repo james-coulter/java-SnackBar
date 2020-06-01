2a1627cd53ed329a9bbd1b97ca6534f997bc679a
james-coulter <95coulter@gmail.com>
2020-06-01 12:55:43 -0600

    Set up initial Main file and figured out compiler. Currently compiling correctly.

---
+++ src/snackBarApp/Main.java
@@ -0,0 +1,12 @@
+package snackBarApp;
+
+public class Main {
+
+	private static void workWithData(){
+		System.out.println("Main Initial Compile Working!");
+	}
+
+	public static void main(String[] args) {
+		workWithData();
+	}
+}
